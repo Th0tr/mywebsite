935913431485c242a86901d834d8680e24a65a21
Th0tr <156580487+Th0tr@users.noreply.github.com>
2024-09-29 14:58:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+// Toggle Nav
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+});
+
+// Link Fade Animation
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+        navLinks.forEach(link => {
+            link.style.animation = '';
+        });
+    });
+});
